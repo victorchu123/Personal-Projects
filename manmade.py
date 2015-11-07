c3da873e398c398c4210df469f00988c6d8dd97e
Arjun Shah <ashah01@wesleyan.edu>
2015-11-07 17:40:36 -0500

    added html scraping functions

--- manmade.py
+++ manmade.py
@@ -1,9 +1,31 @@
 from lxml import html
 import requests
+import re
 
-homepage = requests.get('http://www.wesleyan.edu/filmseries/index.html')
-hometree = html.fromstring(homepage.content)
+#takes index page on wesleyan film series site and returns list of html extensions for dates
+def indexsearch():
+	indexpage = requests.get('http://www.wesleyan.edu/filmseries/index.html')
+	indextree = html.fromstring(indexpage.content)
 
-links = hometree.xpath('//*[@id="content_primary"]/p[3]/a[1]').get("href")
+	linklist = indextree.xpath('//a/@href')
 
-print links
+
+	regex = re.compile('([a-zA-Z]*)\s([0-9]*)\s([0-9]*)')
+
+	filteredlinks = filter(regex.match, linklist)
+
+	reducedlinks = list(set(filteredlinks))
+
+	return reducedlinks
+
+
+#takes an extension and returns a list of triples (date, movie, description)
+def pagesearch(extension):
+	page = requests.get('http://www.wesleyan.edu/filmseries/' + extension)
+	pagetree = html.fromstring(page.content)
+
+	datelist = pagetree.xpath('//*[@class="movie"]/text()')
+	titlelist = pagetree.xpath('//*[@class="movietitle"]/text()')
+	infolist = pagetree.xpath('//*[@class = "indent"]/text()')
+	descriptionlist = pagetree.xpath('//*[@class="indent"]/description/p/text()')
+	return zip(datelist,titlelist,infolist,descriptionlist)
